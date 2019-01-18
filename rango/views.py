5f7ab6f3dee0a45e9a57a393aaff34ef35212da9
Eftychios Karagiorgis <2329664k@student.gla.ac.uk>
2019-01-18 15:54:45 +0000

    Added templates, static and media file. End of chapter 4

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,13 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! <br/><a href='/rango/about/'>About</a>")
+    # construc the context dictionary to be passed to the template
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+
+    # return a rendered response to send to the client
+    return render(request, 'rango/index.html', context=context_dict)
 
 
 def about(request):
